155794473d7736e9712371fab392344eb04a7d86
Martin Bauernschmitt <martin.bauernschmitt@francor.de>
2023-01-13 22:10:55 +0100

    Updated main for testing

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,51 @@
+//use clap::{Arg, ArgAction, Command};
 use frankly_fw_update_cli::francor::franklyboot::{
     com::{can::CANInterface, serial::SerialInterface},
     device::Device,
     firmware::hex_file::HexFile,
-    Error,
 };
 
-//#[test]
-//fn device_flash() {
-//    let firmware = HexFile::from_file("./tests/data/TestFirmware.hex").unwrap();
-//    let mut device = Device::new();
-//    let mut com = new_com_sim_with_data();
-//
-//    device.init(&mut com).unwrap();
-//
-//    device.flash(&mut com, &firmware).unwrap();
-//}
+pub enum InterfaceType {
+    Serial,
+    CAN,
+    Ethernet,
+}
+
+pub fn search_for_devices(interface_type: InterfaceType, interface_name: &String) {
+    match interface_type {
+        InterfaceType::Serial => {
+            println!("Searching for devices on serial port {}", interface_name);
+        }
+        InterfaceType::CAN => {
+            println!("Searching for devices on CAN bus {}", interface_name);
+        }
+        InterfaceType::Ethernet => {
+            println!("Searching for devices on Ethernet {}", interface_name);
+        }
+    }
+}
+
+pub fn run_can_test() {
+    let node_lst = CANInterface::ping_network("can0").unwrap();
+
+    println!("Found nodes: {:?}", node_lst);
+
+    let mut device = Device::new(CANInterface::open("can0").unwrap());
+    device.init().unwrap();
+    device.erase().unwrap();
+}
+
+pub fn run_serial_test() {
+    let mut device = Device::new(SerialInterface::open("/dev/ttyACM0", 115200).unwrap());
+    device.init().unwrap();
+    device.erase().unwrap();
+
+    device
+        .flash(&HexFile::from_file("./tests/data/example_app_g431rb.hex").unwrap())
+        .unwrap();
+}
 
 fn main() {
-    // Create new device
-    let mut device = Device::new();
-
-    // Create new serial interface
-    let mut com = SerialInterface::open("/dev/ttyACM0", 115200).unwrap();
-    //let mut com = CANInterface::open("can0").unwrap();
-
-    device.init(&mut com).unwrap();
-    println!("Device: {}", device);
-
-    //device.erase(&mut com).unwrap();
-
-    // Open firmware file
-    let firmware = HexFile::from_file("./tests/data/example_app_g431rb.hex").unwrap();
-    //let firmware = HexFile::from_file("./tests/data/TestG431RBBlinky.hex").unwrap();
-
-    // Flash firmware
-    device.flash(&mut com, &firmware).unwrap();
+    // run_can_test();
+    run_serial_test();
 }

469178aa9184384a863cf45ecc8e2c7c09862f78
Martin Bauernschmitt <martin.bauernschmitt@francor.de>
2023-08-26 11:59:00 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,151 @@ impl InterfaceType {
     }
 }
 
+fn main() {
+    create_sim_devices();
+
+    let type_arg = Arg::new("type")
+        .long("type")
+        .help("Interface type \"sim\", \"serial\", \"can\"")
+        .required(true)
+        .action(ArgAction::Set)
+        .num_args(1);
+
+    let interface_arg = Arg::new("interface")
+        .long("interface")
+        .help("Interface name \"can0\", \"ttyACM0\", \"sim\"")
+        .required(true)
+        .action(ArgAction::Set)
+        .num_args(1);
+
+    let node_arg = Arg::new("node")
+        .long("node")
+        .help("Node ID: 1, 2, ..")
+        .value_parser(clap::value_parser!(u8).range(0..))
+        .required(false)
+        .action(ArgAction::Set)
+        .num_args(1);
+
+    let matches = Command::new("frankly-fw-update")
+        .version("0.1.0")
+        .author("Martin Bauernschmitt - FRANCOR e.V.")
+        .arg_required_else_help(true)
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("search")
+                .long_flag("search")
+                .about("Search for connected devices on specified network")
+                .arg(type_arg.clone())
+                .arg(interface_arg.clone()),
+        )
+        .subcommand(
+            Command::new("erase")
+                .long_flag("erase")
+                .about("Erases the application from the device")
+                .arg(type_arg.clone())
+                .arg(interface_arg.clone())
+                .arg(node_arg.clone()),
+        )
+        .subcommand(
+            Command::new("flash")
+                .long_flag("flash")
+                .about("Flashes the application to the device")
+                .arg(type_arg.clone())
+                .arg(interface_arg.clone())
+                .arg(node_arg.clone())
+                .arg(
+                    Arg::new("hex-file")
+                        .long("hex-file")
+                        .help("Path to hex file")
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .num_args(1),
+                ),
+        )
+        .get_matches();
+
+    println!("Frankly Firmware Update CLI (c) 2023 Martin Bauernschmitt - FRANCOR e.V.");
+
+    match matches.subcommand() {
+        Some(("search", search_matches)) => {
+            let interface_type_str = search_matches.get_one::<String>("type").unwrap();
+            let interface_type = InterfaceType::from_str(&interface_type_str).unwrap();
+            let interface_name = search_matches.get_one::<String>("interface").unwrap();
+
+            match interface_type {
+                InterfaceType::Serial => search_for_devices::<SerialInterface>(
+                    &ComConnParams::for_serial_conn(interface_name, 115200),
+                ),
+                InterfaceType::CAN => {
+                    search_for_devices::<CANInterface>(&ComConnParams::for_can_conn(interface_name))
+                }
+                InterfaceType::Ethernet => {
+                    println!("Ethernet not supported yet");
+                }
+                InterfaceType::Sim => {
+                    search_for_devices::<SIMInterface>(&ComConnParams::for_sim_device())
+                }
+            }
+        }
+        Some(("erase", erase_matches)) => {
+            let interface_type_str = erase_matches.get_one::<String>("type").unwrap();
+            let interface_type = InterfaceType::from_str(interface_type_str).unwrap();
+            let interface_name = erase_matches.get_one::<String>("interface").unwrap();
+            let node_id = match erase_matches.get_one::<u8>("node") {
+                Some(v) => Some(*v),
+                None => None,
+            };
+
+            match interface_type {
+                InterfaceType::Serial => erase_device::<SerialInterface>(
+                    &ComConnParams::for_serial_conn(interface_name, 115200),
+                    node_id,
+                ),
+                InterfaceType::CAN => erase_device::<CANInterface>(
+                    &ComConnParams::for_can_conn(interface_name),
+                    node_id,
+                ),
+                InterfaceType::Ethernet => println!("Ethernet not supported yet"),
+                InterfaceType::Sim => {
+                    erase_device::<SIMInterface>(&ComConnParams::for_sim_device(), node_id)
+                }
+            }
+        }
+        Some(("flash", flash_matches)) => {
+            let interface_type_str = flash_matches.get_one::<String>("type").unwrap();
+            let interface_type = InterfaceType::from_str(interface_type_str).unwrap();
+            let interface_name = flash_matches.get_one::<String>("interface").unwrap();
+            let node_id = match flash_matches.get_one::<u8>("node") {
+                Some(v) => Some(*v),
+                None => None,
+            };
+            let hex_file_path = flash_matches.get_one::<String>("hex-file").unwrap();
+
+            match interface_type {
+                InterfaceType::Serial => flash_device::<SerialInterface>(
+                    &ComConnParams::for_serial_conn(interface_name, 115200),
+                    node_id,
+                    &hex_file_path,
+                ),
+                InterfaceType::CAN => flash_device::<CANInterface>(
+                    &ComConnParams::for_can_conn(interface_name),
+                    node_id,
+                    &hex_file_path,
+                ),
+                InterfaceType::Ethernet => println!("Ethernet not supported yet"),
+                InterfaceType::Sim => flash_device::<SIMInterface>(
+                    &ComConnParams::for_sim_device(),
+                    node_id,
+                    &hex_file_path,
+                ),
+            }
+        }
+        _ => {
+            println!("Unknown command");
+        }
+    }
+}
+
 pub fn connect_device<I>(
     conn_params: &ComConnParams,
     node_id: Option<u8>,
@@ -71,34 +216,32 @@ where
     }
 }
 
-pub fn erase_device<I>(conn_params: &ComConnParams, node_id: u8)
+pub fn erase_device<I>(conn_params: &ComConnParams, node_id: Option<u8>)
 where
     I: ComInterface,
 {
-    let node_id = {
-        if I::is_network() {
-            Some(node_id)
-        } else {
-            None
+    if I::is_network() {
+        if node_id.is_none() {
+            println!("Node ID required for multi device network interface! Specify with --node <node-id>");
+            return;
         }
-    };
+    }
 
     let mut device = connect_device::<I>(conn_params, node_id).unwrap();
     println!("Device: {}", device);
     device.erase().unwrap();
 }
 
-pub fn flash_device<I>(conn_params: &ComConnParams, node_id: u8, hex_file_path: &str)
+pub fn flash_device<I>(conn_params: &ComConnParams, node_id: Option<u8>, hex_file_path: &str)
 where
     I: ComInterface,
 {
-    let node_id = {
-        if I::is_network() {
-            Some(node_id)
-        } else {
-            None
+    if I::is_network() {
+        if node_id.is_none() {
+            println!("Node ID required for multi device network interface! Specify with --node <node-id>");
+            return;
         }
-    };
+    }
 
     let mut device = connect_device::<I>(conn_params, node_id).unwrap();
     println!("Device: {}", device);
@@ -110,151 +253,6 @@ where
 fn create_sim_devices() {
     let node_lst = SIM_NODE_LST.to_vec();
     SIMInterface::config_nodes(node_lst).unwrap();
-}
-
-fn main() {
-    create_sim_devices();
-
-    let type_arg = Arg::new("type")
-        .short('t')
-        .long("type")
-        .help("Interface type \"sim\", \"serial\", \"can\"")
-        .required(true)
-        .action(ArgAction::Set)
-        .num_args(1);
-
-    let interface_arg = Arg::new("interface")
-        .short('i')
-        .long("interface")
-        .help("Interface name \"can0\", \"ttyACM0\", \"sim\"")
-        .required(true)
-        .action(ArgAction::Set)
-        .num_args(1);
-
-    let node_arg = Arg::new("node")
-        .short('n')
-        .long("node")
-        .help("Node ID: 0, 1, ..")
-        .value_parser(clap::value_parser!(u8).range(0..))
-        .required(true)
-        .action(ArgAction::Set)
-        .num_args(1);
-
-    let matches = Command::new("frankly-fw-update")
-        .version("0.1.0")
-        .author("Martin Bauernschmitt - FRANCOR e.V.")
-        .arg_required_else_help(true)
-        .subcommand_required(true)
-        .subcommand(
-            Command::new("search")
-                .short_flag('s')
-                .long_flag("search")
-                .about("Search for connected devices on specified network")
-                .arg(type_arg.clone())
-                .arg(interface_arg.clone()),
-        )
-        .subcommand(
-            Command::new("erase")
-                .short_flag('e')
-                .long_flag("erase")
-                .about("Erases the application from the device")
-                .arg(type_arg.clone())
-                .arg(interface_arg.clone())
-                .arg(node_arg.clone()),
-        )
-        .subcommand(
-            Command::new("flash")
-                .short_flag('f')
-                .long_flag("flash")
-                .about("Flashes the application to the device")
-                .arg(type_arg.clone())
-                .arg(interface_arg.clone())
-                .arg(node_arg.clone())
-                .arg(
-                    Arg::new("hex-file")
-                        .long("hex-file")
-                        .help("Path to hex file")
-                        .required(true)
-                        .action(ArgAction::Set)
-                        .num_args(1),
-                ),
-        )
-        .get_matches();
-
-    println!("Frankly Firmware Update CLI (c) 2021 Martin Bauernschmitt - FRANCOR e.V.");
-
-    match matches.subcommand() {
-        Some(("search", search_matches)) => {
-            let interface_type_str = search_matches.get_one::<String>("type").unwrap();
-            let interface_type = InterfaceType::from_str(&interface_type_str).unwrap();
-            let interface_name = search_matches.get_one::<String>("interface").unwrap();
-
-            match interface_type {
-                InterfaceType::Serial => search_for_devices::<SerialInterface>(
-                    &ComConnParams::for_serial_conn(interface_name, 115200),
-                ),
-                InterfaceType::CAN => {
-                    search_for_devices::<CANInterface>(&ComConnParams::for_can_conn(interface_name))
-                }
-                InterfaceType::Ethernet => {
-                    println!("Ethernet not supported yet");
-                }
-                InterfaceType::Sim => {
-                    search_for_devices::<SIMInterface>(&ComConnParams::for_sim_device())
-                }
-            }
-        }
-        Some(("erase", erase_matches)) => {
-            let interface_type_str = erase_matches.get_one::<String>("type").unwrap();
-            let interface_type = InterfaceType::from_str(interface_type_str).unwrap();
-            let interface_name = erase_matches.get_one::<String>("interface").unwrap();
-            let node_id = *erase_matches.get_one::<u8>("node").unwrap();
-
-            match interface_type {
-                InterfaceType::Serial => erase_device::<SerialInterface>(
-                    &ComConnParams::for_serial_conn(interface_name, 115200),
-                    node_id,
-                ),
-                InterfaceType::CAN => erase_device::<CANInterface>(
-                    &ComConnParams::for_can_conn(interface_name),
-                    node_id,
-                ),
-                InterfaceType::Ethernet => println!("Ethernet not supported yet"),
-                InterfaceType::Sim => {
-                    erase_device::<SIMInterface>(&ComConnParams::for_sim_device(), node_id)
-                }
-            }
-        }
-        Some(("flash", flash_matches)) => {
-            let interface_type_str = flash_matches.get_one::<String>("type").unwrap();
-            let interface_type = InterfaceType::from_str(interface_type_str).unwrap();
-            let interface_name = flash_matches.get_one::<String>("interface").unwrap();
-            let node_id = *flash_matches.get_one::<u8>("node").unwrap();
-            let hex_file_path = flash_matches.get_one::<String>("hex-file").unwrap();
-
-            match interface_type {
-                InterfaceType::Serial => flash_device::<SerialInterface>(
-                    &ComConnParams::for_serial_conn(interface_name, 115200),
-                    node_id,
-                    &hex_file_path,
-                ),
-                InterfaceType::CAN => flash_device::<CANInterface>(
-                    &ComConnParams::for_can_conn(interface_name),
-                    node_id,
-                    &hex_file_path,
-                ),
-                InterfaceType::Ethernet => println!("Ethernet not supported yet"),
-                InterfaceType::Sim => flash_device::<SIMInterface>(
-                    &ComConnParams::for_sim_device(),
-                    node_id,
-                    &hex_file_path,
-                ),
-            }
-        }
-        _ => {
-            println!("Unknown command");
-        }
-    }
 }
 
 // Tests ------------------------------------------------------------------------------------------
